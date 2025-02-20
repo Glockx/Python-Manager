fd65eca74ee53d4fabc8019235eab8ff98022c30
Nicat Muzaffarli <nicat754@gmail.com>
2025-02-20 10:04:38 +0900

    + disable admin rights during pyenv-win installation

--- src/pythonInstaller.ts
+++ src/pythonInstaller.ts
@@ -114,6 +114,7 @@ export class PythonInstaller {
    */
   private async installPyenvWin(): Promise<void> {
     console.log("Installing pyenv-win on Windows...");
+    await this.disableAdminRights();
     return new Promise((resolve, reject) => {
       // This PowerShell command downloads and runs the pyenv-win installer script.
       const psCommand = `Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"`;
